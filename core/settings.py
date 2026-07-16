c39ee91cb4e2081d7c5b97eb752ff4804e57abad
Parisa-Reza <rezaparisa5@gmail.com>
2026-07-16 16:46:42 +0600

    feat: add chat application with forms and URL routing

--- core/settings.py
+++ core/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat'
 ]
 
 MIDDLEWARE = [
@@ -100,3 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
